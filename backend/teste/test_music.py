6c782799d5bf80d4f1d640df2861aa190827102f
NatanRib12 <natansantosribeiro241@gmail.com>
2025-10-27 10:32:41 -0300

    implementando o código completo test_user

--- backend/teste/test_music.py
+++ backend/teste/test_music.py
@@ -1,4 +1,5 @@
 from fastapi.testclient import TestClient
+from fastapi import FastAPI
 from app.main import app
 from app.routers import music 
 
--- backend/teste/test_user.py
+++ backend/teste/test_user.py
@@ -10,3 +10,13 @@ def test_create_user():
     if response.status_code != 200:
         print(f" Erro {response.status_code} ao criar usuário.")
         print("Resposta completa:", response.json())
+
+        assert response.status_code == 200
+    data = response.json()
+
+    assert data["name"] == "Maria Silva"
+    assert data["email"] == "maria@gmail.com"
+    assert data["username"] == "mariazinha"
+    assert data["password_hash"] == "senha123"
+    assert "id" in data
+    assert isinstance(data["id"], int)
